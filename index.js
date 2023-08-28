94f6f1605bfcc6ae7f487cdc57778ee992c200fb
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-28 22:57:23 +0530

    Configured successfully for nodejs

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.all("*", (req, res) => {
+  res.send("Page Not Found!!");
 });
 
 app.listen(3000, () => {
