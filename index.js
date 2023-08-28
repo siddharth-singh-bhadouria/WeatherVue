d454f545931939728b7384d1cc3ceac65f1ebc12
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-29 00:48:54 +0530

    Completed the api functioning and app design

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const path = require("path");
 const app = express();
+const axios = require("axios");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -12,16 +13,26 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const weather = await axios.get(
+    `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=London`
+  );
+  const location = "London";
+  const { temp_c, humidity, wind_kph } = weather.data.current;
+  const { text, icon } = weather.data.current.condition;
+  res.render("index", { location, temp_c, humidity, wind_kph, text, icon });
 });
 
-// const externalLink =
-
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const { location } = req.body;
-  const weather = `http://api.weatherapi.com/v1/current.json?key=API_KEY&q=${location}`;
-  console.log(weather);
+  const weather = await axios.get(
+    `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${location}`
+  );
+  const { temp_c, humidity, wind_kph } = weather.data.current;
+  const { text, icon } = weather.data.current.condition;
+
+  res.render("index", { location, temp_c, humidity, wind_kph, text, icon });
+  console.log(temp_c, text, icon, humidity, wind_kph);
 });
 
 app.all("*", (req, res) => {
