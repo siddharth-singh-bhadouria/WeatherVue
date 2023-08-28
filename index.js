b456b534f3d895d5b1fd003a569027fb8f41d421
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-28 22:24:01 +0530

    Express added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING TO PORT 3000!!");
+});
